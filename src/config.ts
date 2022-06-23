26e25bf436fc7d51f20b4128faf7e4c7929ea66f
hiddenderek <hiddenderek@gmail.com>
2022-06-22 21:26:39 -0500

    fixed CORS issue

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,7 @@
 let port = {
     port: typeof process != "undefined" ? (process?.env?.PORT || 8030) : 8030,
     authPort: typeof process != "undefined" ? (process?.env?.PORT || 8020) : 8020,
-    hostname: typeof window != "undefined" ? window.location.hostname : "localhost"
+    hostname: typeof window != "undefined" ? window.location.hostname : "159.223.199.71"
 }
 export default port
 
